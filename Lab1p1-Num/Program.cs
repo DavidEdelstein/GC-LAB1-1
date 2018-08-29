06f09a2b0e49e0953e73f0e2003ae2c24bfe6d97
David Edelstein <David Edelstein>
2018-08-29 19:28:16 -0400

    1st repository

---
+++ Lab1p1-Num/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Methods of the different components to the code, look below to see the method breakdown--
+
+            Tuple<int, int> userInput = GetUserInput();
+            
+            List<int> listnum1 = SeperateNumberPlaces(userInput.Item1);
+            List<int> listnum2 = SeperateNumberPlaces(userInput.Item2);
+
+            string answer = NumberPlacesEqual(listnum1, listnum2);
+
+            Console.WriteLine(answer);
+
+
+            //This is how the code would look without the use of the methods above.
+
+            /*Console.WriteLine("Enter Number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            return new Tuple<int, int>(number1, number2);*/
+
+            /*List<int> listnum1 = new List<int>();
+            while (number1 > 0)
+            {
+                listnum1.Add(number1 % 10);
+                number1 = number1 / 10;
+            }
+
+            List<int> listnum2 = new List<int>();
+            while (number2 > 0)
+            {
+                listnum2.Add(number2 % 10);
+                number2 = number2 / 10;
+            }*/
+
+
+            /* String result = "";
+             for (int i = 0; i < listnum1.Count - 1; i++)
+             {
+                 if (listnum1[i] + listnum2[i] == listnum1[i + 1] + listnum2[i + 1] && result != "False")
+                 {
+                     result = "True";
+                 }
+                 else
+                 {
+                     result = "False";
+                 }
+
+             }
+             Console.WriteLine(result);*/
+
+            Console.ReadLine();
+        }
+
+
+        //Code block #1 Get the users input--
+        public static Tuple<int, int> GetUserInput()
+        {
+            // get first number from user--
+            Console.WriteLine("Enter Number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+           
+            // get second number from user--
+            Console.WriteLine("Enter Number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            
+            //Return is to return the method result to the console--
+            return new Tuple<int, int>(number1, number2);
+
+        }
+
+        // Code block #2 Seperate input number places (eg; 1's, 10's, 100's)
+        
+        public static List<int> SeperateNumberPlaces(int num)
+        {
+            // Create a list that holds the functions to be able to seperate the coresponding number places--
+            List<int> listnum1 = new List<int>();
+            
+            // While loop needed to seperate more than single digit numbers.
+            // Loop has to solve the equations until the int 0, num>0--
+            while (num > 0)
+            {
+                listnum1.Add(num % 10);
+                num = num / 10;
+            }
+            return listnum1;
+        }
+
+        /* Code block #3 Take each individual numbers that were seperated from the while loop and add them to 
+           their coressponding number places and if the sum of all numbers equal then program will print out 
+           True if any one of the sums do not equal then program will print false.*/
+
+        public static string NumberPlacesEqual(List<int> listnum1, List<int> listnum2)
+        {
+            // Employ an array
+            String result = "";
+            for (int i = 0; i < listnum1.Count - 1; i++)
+            {
+                //if else will set up the result if the conditions are equal or not--
+                if (listnum1[i] + listnum2[i] == listnum1[i + 1] + listnum2[i + 1] && result != "False")
+                {
+                    result = "True";
+                }
+                else
+                {
+                    result = "False";
+                }
+            }
+            return result;
+        }
+    }
+
+}
---
+++ Lab1p1-Num/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hello World app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Hello World app")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74318a84-3bf5-4eae-8ab7-99b09d2f2d52")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
